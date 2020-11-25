19304f65f4c2fffd5a0185569bb630dac262a4a1
Marìa Carolina Gòmez <mayravargase01@gmail.com>
2020-11-24 19:25:29 -0500

    Java 24-11-2020

---
+++ main.js
@@ -0,0 +1,48 @@
+//Estos Fragmentos de codigo es para visualizar
+// console.log("soy el archivo MediaDeviceInfo.js conectado exitosamente");
+// alert("soy el archivo MediaDeviceInfo.js conectado exitosamente");//
+
+// let nombre_usuario;
+// nombre_usuario = prompt("Ingrese su nombre");
+// console.log("nombre de usuario es:" ,nombre_usuario);
+
+
+
+
+
+// let numero_1 = 5;
+// let numero_2 = 10;
+
+// console.log("contenido de la variable 1 nombre de la variable numero1", numero_1);
+// console.log("contenido de la variable 2 nombre de la variable numero2", numero_2);
+
+
+// numero_1 = numero_2
+
+// console.log("contenido de la variable 1 numero_1 contine lo de la variable numero_2", numero_2);
+
+
+// let numero_1 = 1;
+// let numero_2 = 25;
+// let resultado;
+
+// console.log(numero_1);
+// numero_1 += numero_2;
+// resultado = numero_1 = numero_2;
+
+// let numero_1 = 1;
+// let numero_2 = 25;
+// let resultado;
+
+// console.log(numero_1);
+// numero_1 -= numero_2;
+// resultado = numero_1 = numero_2;
+
+let numero_1 = 1;
+let numero_2 = 25;
+let resultado;
+
+console.log(numero_1);
+numero_2 %= numero_1;
+resultado = numero_1 = numero_2;
+
